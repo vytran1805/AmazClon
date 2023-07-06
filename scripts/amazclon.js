c2d675856d78381d9a3a3c48b9939cff1c384b33
VyT <vytran1805@gmail.com>
2023-07-06 16:11:06 -0400

    Import products.js to amazclon.js

--- scripts/amazclon.js
+++ scripts/amazclon.js
@@ -1,57 +1,57 @@
 // Create a list of products
-const product = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
+// const product = [
+//   {
+//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//     rating: {
+//       stars: 4.5,
+//       count: 87,
+//     },
+//     priceCents: 1090,
+//   },
+//   {
+//     image: "images/products/intermediate-composite-basketball.jpg",
+//     name: "Intermediate Size Basketball",
+//     rating: {
+//       stars: 4,
+//       count: 127,
+//     },
+//     priceCents: 2095,
+//   },
+//   {
+//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//     name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//     rating: {
+//       stars: 4.5,
+//       count: 56,
+//     },
+//     priceCents: 799,
+//   },
+// ];
+import { products } from "../data/products.js";
 // generate html for each of the products using loop
 let productHTML = "";
-for (let i = 0; i < product.length; i++) {
+for (let i = 0; i < products.length; i++) {
   productHTML += `<div class="product-container">
     <div class="product-image-container">
       <img class="product-image"
-        src="${product[i].image}">
+        src="${products[i].image}">
     </div>
 
     <div class="product-name limit-text-to-2-lines">
-      ${product[i].name}
+      ${products[i].name}
     </div>
 
     <div class="product-rating-container">
       <img class="product-rating-stars"
-        src="images/ratings/rating-${product[i].rating.stars * 10}.png">
+        src="images/ratings/rating-${products[i].rating.stars * 10}.png">
       <div class="product-rating-count link-primary">
-        ${product[i].rating.count}
+        ${products[i].rating.count}
       </div>
     </div>
 
     <div class="product-price"> 
-      $${(product[i].priceCents / 100).toFixed(2)} 
+      $${(products[i].priceCents / 100).toFixed(2)} 
     </div>
 
     <div class="product-quantity-container">
